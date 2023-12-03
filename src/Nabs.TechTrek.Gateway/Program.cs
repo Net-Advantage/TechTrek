21f697a57de5c6088070ec25db4dd258590d1a34
taxlabDarrel <darrel@taxlab.online>
2023-12-03 22:14:32 +1300

    Yarp is now working with OIDC using AddMicrosoftIdentityWebApp

--- src/Nabs.TechTrek.Gateway/Program.cs
+++ src/Nabs.TechTrek.Gateway/Program.cs
@@ -1,6 +1,20 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Identity.Web;
+using Nabs.TechTrek.Gateway.Middlewares;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
+
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+builder.Services
+	.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+	.AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"));
+
+builder.Services
+	.AddAuthorization();
 
 builder.Services
 	.AddReverseProxy()
@@ -9,8 +23,19 @@ builder.Services
 
 var app = builder.Build();
 
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
 app.MapDefaultEndpoints();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseMiddleware<TechTrekCookieAuthMiddleware>();
 
 app.MapReverseProxy();
 
 app.Run();
+
+
+class TechTrekUser : IdentityUser;
