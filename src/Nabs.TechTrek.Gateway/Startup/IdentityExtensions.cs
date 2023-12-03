e39c581496dd6d4a98938b3d7c51dba4e25162b6
taxlabDarrel <darrel@taxlab.online>
2023-12-04 00:18:23 +1300

    Cleanup. Ready for the next phase.

---
+++ src/Nabs.TechTrek.Gateway/Startup/IdentityExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Identity.Web;
+
+namespace Nabs.TechTrek.Gateway.Startup;
+
+public static class IdentityExtensions
+{
+	public static WebApplicationBuilder AddIdentity(this WebApplicationBuilder builder)
+	{
+		builder.Services
+			.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+			.AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"));
+
+		builder.Services
+			.AddAuthorization();
+
+		return builder;
+	}
+}
