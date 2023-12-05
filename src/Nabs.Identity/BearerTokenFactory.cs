f7926bc71aaf2cd222f3e83285a1dd7811c8d905
taxlabDarrel <darrel@taxlab.online>
2023-12-05 23:45:32 +1300

    More updates to get the Identity gateways working.

---
+++ src/Nabs.Identity/BearerTokenFactory.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security;
+using System.Security.Claims;
+using System.Text;
+
+namespace Nabs.Identity;
+
+public sealed class BearerTokenFactory(BearerTokenSettings bearerTokenSettings)
+{
+	private readonly BearerTokenSettings _bearerTokenSettings = bearerTokenSettings;
+
+	public string GenerateTokenFromClaims(IEnumerable<Claim> claims)
+	{
+		var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_bearerTokenSettings.Secret));
+		var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+		var token = new JwtSecurityToken(
+						issuer: _bearerTokenSettings.Issuer,
+						audience: _bearerTokenSettings.Audience,
+						claims: claims,
+						expires: DateTime.UtcNow.AddMinutes(30),
+						signingCredentials: credentials);
+		try
+		{
+			var result = new JwtSecurityTokenHandler().WriteToken(token);
+			return result;
+		}
+		catch(Exception ex)
+		{
+			return $"InvalidToken: {ex.Message}";
+		}
+	}
+}
