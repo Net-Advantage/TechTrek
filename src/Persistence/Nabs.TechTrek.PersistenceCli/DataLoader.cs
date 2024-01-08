79b9dbf136bd678ebfcfd8bf9bdb8db9e6980e79
taxlabDarrel <darrel@taxlab.online>
2024-01-09 12:49:10 +1300

    Playing with containers in workflows.

---
+++ src/Persistence/Nabs.TechTrek.PersistenceCli/DataLoader.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Nabs.TechTrek.Persistence;
+
+namespace Nabs.TechTrek.PersistenceCli;
+
+internal sealed class DataLoader(IDbContextFactory<TechTrekDedicatedTenantDbContext> dbContextFactory)
+{
+    private readonly IDbContextFactory<TechTrekDedicatedTenantDbContext> _dbContextFactory = dbContextFactory;
+
+    internal async Task EnsureDatabaseCreatedAsync()
+    {
+        var dbContext = _dbContextFactory.CreateDbContext();
+        await dbContext.Database.EnsureDeletedAsync();
+        await dbContext.Database.EnsureCreatedAsync();
+    }
+
+    internal async Task LoadScenarioDataAsync()
+    {
+        var dbContext = _dbContextFactory.CreateDbContext();
+
+
+
+        await Task.CompletedTask;
+    }
+}
