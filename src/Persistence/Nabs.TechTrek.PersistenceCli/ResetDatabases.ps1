3c1fbe4ea4c274081d7f38224ec3b14af531dcf4
taxlabDarrel <darrel@taxlab.online>
2024-01-10 16:53:39 +1300

    Really get it right.

--- src/Persistence/Nabs.TechTrek.PersistenceCli/ResetDatabases.ps1
+++ src/Persistence/Nabs.TechTrek.PersistenceCli/ResetDatabases.ps1
@@ -1,6 +1,10 @@
+cd .\src\Persistence\Nabs.TechTrek.PersistenceCli\
+
 dotnet run -- Reset -i SharedShared -t 00000000-0000-0000-0000-000000000000
 dotnet run -- Load -i SharedShared -t 731724a1-9b57-46ce-baaf-7325bc8711c0
 dotnet run -- Load -i SharedShared -t 931d3b9a-4931-4577-bbe0-dc913db3d3c9
 
 dotnet run -- Reset -i DedicatedDedicated -t 731724a1-9b57-46ce-baaf-7325bc8711c0
 dotnet run -- Load -i DedicatedDedicated -t 731724a1-9b57-46ce-baaf-7325bc8711c0
+
+cd ..\..\..\
