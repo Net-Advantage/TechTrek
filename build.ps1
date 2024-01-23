75fcdab7c045f43fa93a74892580720fae98f376
dwschreyer <dwschreyer@live.com>
2024-01-24 11:26:03 +1300

    The most important changes include the update of the `build.ps1` script to include definitions for paths to coverage reports and test results, a cleanup process for old test results and coverage reports, and a command to generate a coverage report using the ReportGenerator tool. Additionally, the `Nabs.TechTrek.sln` solution file has been updated to include references to the `build.ps1` script and the GitHub workflows configuration file `build.yml`.
    List of changes:
    
    1. The `build.ps1` script has been updated to include definitions for paths to coverage reports and test results. This change allows the script to locate and manage these files more efficiently.
    2. A cleanup process for old test results and coverage reports has been added to the `build.ps1` script. This helps to keep the workspace clean and organized.
    3. The `build.ps1` script now includes a command to generate a coverage report using the ReportGenerator tool. This provides a convenient way to generate coverage reports.
    4. The `Nabs.TechTrek.sln` solution file has been updated to include references to the `build.ps1` script. This allows the solution to utilize the updated script.
    5. The `Nabs.TechTrek.sln` solution file now includes a reference to the GitHub workflows configuration file `build.yml`. This integrates the GitHub workflows into the solution.

--- build.ps1
+++ build.ps1
@@ -1,6 +1,19 @@
+# Define paths for coverage reports and test results
+$rootDirectory = Get-Location
+$testResultsPath = "TestResults"
+$coverageReportPath = "coveragereport"
+
+# Clean up old test results and coverage reports
+Get-ChildItem -Path $rootDirectory -Recurse -Filter $testResultsPath -Directory | ForEach-Object {
+    Remove-Item -Path $_.FullName -Recurse -Force
+    Write-Host "Removed TestResults directory at: $($_.FullName)"
+}
+
+if (Test-Path $coverageReportPath) {
+    Remove-Item -Path $coverageReportPath -Recurse -Force
+}
+
 dotnet restore src/Nabs.TechTrek.sln --configfile ./src/nuget.config
 dotnet build src/Nabs.TechTrek.sln --configuration Release --no-restore
-& "./src/Persistence/Nabs.TechTrek.PersistenceCli/ResetDatabases.ps1"
 dotnet test src/Nabs.TechTrek.sln --configuration Release --no-restore --no-build --logger "console;verbosity=detailed" --settings src/coverlet.runsettings
-dotnet test src/Nabs.TechTrek.sln --settings src/coverlet.runsettings
 & "$env:UserProfile/.nuget/packages/reportgenerator/5.2.0/tools/net8.0/ReportGenerator.exe" -reports:"**/TestResults/*/coverage.cobertura.xml" -targetdir:"coveragereport"
